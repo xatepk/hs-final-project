4306f7b32abe08283d4fe23433f5058e152c50d3
xatepk <kkhatya@gmail.com>
2022-11-24 00:40:28 +0400

    newsDetails

--- frontend/src/components/NewsCard.tsx
+++ frontend/src/components/NewsCard.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { INews } from "../models/models";
 
 interface NewsCardProps {
@@ -5,16 +6,23 @@ interface NewsCardProps {
 }
 
 function NewsCard({ newsCard }: NewsCardProps) {
+  const navigate = useNavigate();
+
+  const openNews = () => {
+    navigate('/news/' + newsCard._id)
+  }
 
   return (
     <li className="news__card">
-      <img src={newsCard.image} alt="фото новости" className="news__image" />
+      <img src={newsCard.image}
+        onClick={openNews}
+        alt="фото новости" className="news__image" />
       <h2 className="news__title">{newsCard.title}</h2>
       <p className="news__subtitle">{newsCard.subtitle}</p>
-      {/* <p className="news__description">{newsCard.description}</p> */}
       <div className="news__bottom">
         <span className="news__date">{newsCard.date}</span>
-        <button className="news__button">Читать</button>
+        <button className="news__button"
+        onClick={openNews}>Читать</button>
       </div>
     </li>
   );
