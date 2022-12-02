117c497d578e04cff93b14f0b380c87af5bdac59
xatepk <kkhatya@gmail.com>
2022-12-02 17:53:46 +0400

    apartments backend

---
+++ backend/models/apartments.js
@@ -0,0 +1,51 @@
+const { Schema, model, default: mongoose } = require('mongoose');
+
+
+const dataSchema = new Schema({
+  imageUrls: [{
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /https?:\/\/(w{1,3}\.)?\S+(#$)?/gm.test(v);
+      },
+      message: (props) => `${props.value} is not a valid news url!`,
+    },
+  }],
+  underground: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
+  area: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
+  location: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 65.00,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    default: [],
+    ref: 'user',
+  }],
+  rooms: {
+    type: Number,
+    required: true,
+    default: 4,
+  },
+  description: {
+    type: String,
+    default: "Какое-то описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание Какое-то описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание"
+  }
+});
+
+module.exports = model('apartments', dataSchema);
---
+++ backend/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 2,
+    required: true,
+    select: false,
+  },
+  avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(v) {
+        return /https?:\/\/(w{1,3}\.)?\S+(#$)?/gm.test(v);
+      },
+      message: 'указан неверный адрес',
+    },
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
