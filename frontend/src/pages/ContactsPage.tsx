e274625ba7c0edaa5e6d3cb43f5d551c914835a6
xatepk <kkhatya@gmail.com>
2023-01-19 15:33:24 +0400

    page contacts

--- frontend/src/pages/ContactsPage.tsx
+++ frontend/src/pages/ContactsPage.tsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+import wa from '../img/icons/contacts/wa.svg';
+import tg from '../img/icons/contacts/tg.svg';
+import mobile from '../img/icons/contacts/mobile.svg';
+import ig from '../img/icons/contacts/ig.svg';
+import vk from '../img/icons/contacts/vk.svg';
+import fb from '../img/icons/contacts/fb.svg';
+
 function ContactsPage() {
   return (
     <section className="contacts">
@@ -9,62 +17,62 @@ function ContactsPage() {
           </p>
           <span className="contacts__geo">220068, РБ, г. Минск, ул. Осипенко, 21, кв.23</span>
           <div className="contacts__connect">
-            <span className="contacts__phone">+375 29 621-48-33</span>
+            <Link to="tel:+375296214833" className="contacts__phone">+375 29 621-48-33</Link>
             <ul className="contacts__conn-list">
               <li className="contacts__conn-item">
-                <img src="" alt="иконка" className="contacts__conn-icon" />
+                <img src={wa} alt="иконка" className="contacts__conn-icon" />
               </li>
               <li className="contacts__conn-item">
-                <img src="" alt="иконка" className="contacts__conn-icon" />
+                <img src={tg} alt="иконка" className="contacts__conn-icon" />
               </li>
               <li className="contacts__conn-item">
-                <img src="" alt="иконка" className="contacts__conn-icon" />
+                <img src={mobile} alt="иконка" className="contacts__conn-icon" />
               </li>
             </ul>
           </div>
-          <span className="contacts__mail">sdaem@sdaem.by</span>
-          <span className="contacts__shedule"> Режим работы: 08:00-22:00</span>
-          <p className="contacts__address">ИП Шушкевич Андрей Викторович УНП 192602485 Минским горисполкомом 10.02.2016
-          </p>
+          <p className="contacts__mail">sdaem@sdaem.by</p>
+          <p className="contacts__shedule"> Режим работы: 08:00-22:00</p>
+          <p className="contacts__address">ИП Шушкевич Андрей Викторович</p>
+          <p className="contacts__address">УНП 192602485 Минским горисполкомом 10.02.2016</p>
           <p className="contacts__info">Администрация сайта не владеет информацией
             о наличии свободных квартир</p>
         </div>
         <div className="contacts__form">
           <form id="feedback-form" method="POST" action="#" name="feedback-form" noValidate>
-            <div className="form-row">
-              <div className="form-group">
-                <label htmlFor="name" className="control-label">Ваше имя</label>
-                <input id="name" type="text" name="name" className="form-control" value="" placeholder="Имя" minLength={2}
+            <div className="contacts__form-row">
+              <div className="contacts__form-group">
+                <label htmlFor="name" className="contacts__control-label">Ваше имя</label>
+                <input id="name" type="text" name="name" className="contacts__form-control" value="" placeholder="Алексей" minLength={2}
                   maxLength={30} required />
               </div>
-              <div className="form-group">
-                <label htmlFor="email" className="control-label">Ваша электронная почта</label>
-                <input id="email" type="email" name="email" required className="form-control" value=""
+              <div className="contacts__form-group">
+                <label htmlFor="email" className="contacts__control-label">Ваша электронная почта</label>
+                <input id="email" type="email" name="email" required className="contacts__form-control" value=""
                   placeholder="Введите" />
               </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="message" className="control-label">Ваше сообщение</label>
-              <textarea id="message" name="message" className="form-control" rows={10}
+            <div className="contacts__form-group">
+              <label htmlFor="message" className="contacts__control-label">Ваше сообщение</label>
+              <textarea id="message" name="message" className="contacts__form-control" rows={10}
                 placeholder="Сообщение" minLength={20} maxLength={500}
                 required></textarea>
               <div className="invalid-feedback"></div>
             </div>
-            <div className="form-submit">
-              <button type="submit">Отправить</button>
+            <div className="contacts__form-submit">
+              <button type="submit" className="contacts__form-button">Отправить</button>
             </div>
           </form>
         </div>
         <div className="contacts__network">
           <ul className="contacts__network-list">
             <li className="contacts__network-item">
-              <img src="" alt="иконка" className="contacts__network-icon" />
+              <img src={ig} alt="иконка" className="contacts__network-icon" />
             </li>
             <li className="contacts__network-item">
-              <img src="" alt="иконка" className="contacts__network-icon" />
+              <img src={vk} alt="иконка" className="contacts__network-icon" />
             </li>
             <li className="contacts__network-item">
-              <img src="" alt="иконка" className="contacts__network-icon" />
+              <img src={fb} alt="иконка" className="contacts__network-icon" />
             </li>
           </ul>
         </div>
