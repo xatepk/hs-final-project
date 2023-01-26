bd2bd3d6257b2ac77b7613f8328b97d0272e0164
xatepk <kkhatya@gmail.com>
2023-01-26 17:58:41 +0400

    page mainpage/mainfilter

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-// import MainPageNavigation from "../components/MainPageNavigation";
+import MainPageNavigation from "../components/MainPageNavigation";
 import MainPageAboutRent from "../components/MainPageAboutRent";
 import MainPageRentGeo from "../components/MainPageRentGeo";
 import MainPageRentPhoto from "../components/MainPageRentPhoto";
@@ -7,14 +7,18 @@ import MainPageRentList from "./MainPageRentList";
 function MainPage() {
   return (
     <section className="mainpage">
-      {/* <h1 className="mainpage__title">Sdaem.by - у нас живут <span className="mainpage__title_yellow">ваши объявления</span></h1> */}
-      {/* <MainPageNavigation /> */}
+      <div className="mainpage__filter-block">
+        <h1 className="mainpage__title">Sdaem.by - у нас живут <span className="mainpage__title_yellow">ваши объявления</span></h1>
+        <MainPageNavigation />
+      </div>
+
       <div className="mainpage__rent">
         <MainPageRentPhoto />
         <MainPageRentList />
       </div>
-        <MainPageRentGeo />
-        <MainPageAboutRent />
+
+      <MainPageRentGeo />
+      <MainPageAboutRent />
     </section>
   );
 }
