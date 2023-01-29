6a6b584d7466c4e6bec30d374555834df4964be7
xatepk <kkhatya@gmail.com>
2023-01-29 15:14:39 +0400

    page mainpage/citiesOption

--- frontend/src/components/MainPageFilter.tsx
+++ frontend/src/components/MainPageFilter.tsx
@@ -1,12 +1,19 @@
-function MainPageFilter() {
+import { ICities } from "../models/models";
+
+interface CitiesProps {
+  cities: ICities[];
+}
+
+function MainPageFilter({ cities }: CitiesProps) {
+
   return (
     <div className="mainpage__filter">
       <ul className="mainpage__filter-list">
         <li className="mainpage__filter-item">
           <h3 className="mainpage__filter-item-title">Город</h3>
           <select name="city" className="mainpage__filter-select">
-            <option disabled selected value="">Выберите</option>
-            <option>Минск</option>
+            <option disabled selected value="" className="mainpage__main-option">Выберите</option>
+            {cities.map((city) => <option key={city._id}>{city.city}</option>)}
           </select>
         </li>
         <li className="mainpage__filter-item">
@@ -19,9 +26,9 @@ function MainPageFilter() {
           <h3 className="mainpage__filter-item-title">Цена за сутки (BYN)</h3>
           <label className="mainpage__filter-label">
             <input className="mainpage__filter-input mainpage__filter-input_mr" placeholder="От"></input>
-          -</label>
+            -</label>
           <label>
-            <input className="mainpage__filter-input mainpage__filter-input_ml"placeholder="До"></input>
+            <input className="mainpage__filter-input mainpage__filter-input_ml" placeholder="До"></input>
           </label>
         </li>
         <li className="mainpage__filter-item mainpage__filter-item_option">Больше опций</li>
