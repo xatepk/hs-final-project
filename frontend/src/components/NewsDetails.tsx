f16f2f257918872cd34e2e71835849fc18a74a32
xatepk <kkhatya@gmail.com>
2023-02-05 23:48:14 +0400

    page readmore

--- frontend/src/components/NewsDetails.tsx
+++ frontend/src/components/NewsDetails.tsx
@@ -6,15 +6,19 @@ interface NewsCardDetailsProps {
 }
 
 
-function NewsDetails({ news }: NewsCardDetailsProps ) {
+function NewsDetails({ news }: NewsCardDetailsProps) {
   return (
     <div className="news-details">
-      <Breadcrumb title="Новости" item={news?.title} link="/news" />
-      <h2 className="news-details__title">{news?.title}</h2>
-      <span className="news-details__date">{news?.date}</span>
-      <img src={news?.image}
-        alt="фото новости" className="news-details__image" />
-      <p className="news-details__description">{news?.description}</p>
+      <div className="news-details_bg-title"></div>
+      <div className="news-details_bg"></div>
+      <div className="news-details__item">
+        <Breadcrumb title="Новости" item={news?.title} link="/news" />
+        <h2 className="news-details__title">{news?.title}</h2>
+        <span className="news-details__date">{news?.date}</span>
+        <img src={news?.image}
+          alt="фото новости" className="news-details__image" />
+        <p className="news-details__description">{news?.description}</p>
+      </div>
     </div>
   );
 }
