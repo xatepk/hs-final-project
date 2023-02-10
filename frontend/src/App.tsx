d57bb1b8e773f9c69e509e487c40f3c58905d844
xatepk <kkhatya@gmail.com>
2023-02-10 09:26:23 +0400

    tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ApartmentsPage from './pages/ApartmentsPage';
 import ApartmentsPageByCity from './pages/ApartmentsPageByCity';
 import AuthPage from './pages/AuthPage';
@@ -9,23 +9,32 @@ import NewsPage from './pages/NewsPage';
 import PageNotFound from './pages/PageNotFound';
 import RegistrationPage from './pages/RegistrationPage';
 import SavedApartments from './pages/SavedApartments';
+import { Provider } from 'react-redux';
+import { setupStore } from './store';
+
+const store = setupStore();
 
 function App() {
   return (
-    <div className='page'>
-      <Routes>
-        <Route path='/' element={<MainPage />}></Route>
-        <Route path='/auth' element={<AuthPage />}></Route>
-        <Route path='/register' element={<RegistrationPage />}></Route>
-        <Route path='/news' element={<NewsPage />}></Route>
-        <Route path='/apartments' element={<ApartmentsPage />}></Route>
-        <Route path='/apartments/:city' element={<ApartmentsPageByCity />}></Route>
-        <Route path='/saved' element={<SavedApartments />}></Route>
-        <Route path='/news/:id' element={<NewsDetailPage />}></Route>
-        <Route path='/contacts' element={<ContactsPage />}></Route>
-        <Route path="*" element={<PageNotFound />}></Route>
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className='page'>
+          <Routes>
+            <Route path='/' element={<MainPage />}></Route>
+            <Route path='/auth' element={<AuthPage />}></Route>
+            <Route path='/register' element={<RegistrationPage />}></Route>
+            <Route path='/news' element={<NewsPage />}></Route>
+            <Route path='/apartments' element={<ApartmentsPage />}></Route>
+            <Route path='/apartments/:city' element={<ApartmentsPageByCity />}></Route>
+            <Route path='/saved' element={<SavedApartments />}></Route>
+            <Route path='/news/:id' element={<NewsDetailPage />}></Route>
+            <Route path='/contacts' element={<ContactsPage />}></Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
+
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import './index.less';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { setupStore } from './store';
-
-const store = setupStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-
-);
+root.render(<App />);
 
